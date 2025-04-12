10311bc4ae91ae2191edda22452fb8a49b50e4b5
athirag30 <athirag3011@gmail.com>
2025-04-12 16:51:59 +0530

    Update august.c

--- august.c
+++ august.c
@@ -1,1 +1,16 @@
-...
+#include <stdio.h>
+#include <string.h>
+void augustEncrypt(char* text) {
+    for(int i = 0; text[i] != '\0'; i++) {
+        if(text[i] >= 'A' && text[i] <= 'Z')
+            text[i] = ((text[i] - 'A' + 1) % 26) + 'A';
+        else if(text[i] >= 'a' && text[i] <= 'z')
+            text[i] = ((text[i] - 'a' + 1) % 26) + 'a';
+    }
+}
+int main() {
+    char text[] = "HELLO";
+    augustEncrypt(text);
+    printf("Encrypted: %s\n", text);
+    return 0;
+}
