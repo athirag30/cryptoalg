040e19dfb9c12d7531b8ec010b78e1a19b5909d6
athirag30 <athirag3011@gmail.com>
2025-04-12 16:52:36 +0530

    Update atbash.c

--- atbash.c
+++ atbash.c
@@ -1,1 +1,15 @@
-...
+#include <stdio.h>
+void atbashEncrypt(char* text) {
+    for(int i = 0; text[i] != '\0'; i++) {
+        if(text[i] >= 'A' && text[i] <= 'Z')
+            text[i] = 'Z' - (text[i] - 'A');
+        else if(text[i] >= 'a' && text[i] <= 'z')
+            text[i] = 'z' - (text[i] - 'a');
+    }
+}
+int main() {
+    char text[] = "HELLO";
+    atbashEncrypt(text);
+    printf("Encrypted: %s\n", text);
+    return 0;
+}
