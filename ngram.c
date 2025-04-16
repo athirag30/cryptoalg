f70635c7dff46785459a127481257b4f1f35c51d
athirag30 <athirag3011@gmail.com>
2025-04-16 11:49:56 +0530

    Update ngram.c

--- ngram.c
+++ ngram.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-void ngram(char* text, int n) {
-    int len = strlen(text);
-    for(int i = 0; i <= len - n; i++) {
-        for(int j = 0; j < n; j++) {
-            printf("%c", text[i + j]);
-        }
-        printf("\n");
+
+void ngramEncrypt(char* text) {
+    for(int i = 0; text[i] != '\0'; i += 2) {
+        if(text[i + 1] == '\0') break;
+        char temp = text[i];
+        text[i] = text[i + 1];
+        text[i + 1] = temp;
     }
 }
+
 int main() {
-    char text[] = "HELLOWORLD";
-    int n = 3;
-    ngram(text, n);
+    char text[100];
+    printf("Enter text to encrypt (even length): ");
+    fgets(text, sizeof(text), stdin);
+    text[strcspn(text, "\n")] = '\0';
+    ngramEncrypt(text);
+    printf("Encrypted: %s\n", text);
     return 0;
 }
