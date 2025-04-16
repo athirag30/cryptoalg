fa15dc8f369087c3ebcc4502c030641241f1a9cd
athirag30 <athirag3011@gmail.com>
2025-04-16 11:49:31 +0530

    Update autokey.c

--- autokey.c
+++ autokey.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 #include <string.h>
-void autokeyEncrypt(char* text, char* key) {
-    int n = strlen(text);
-    char new_key[100];
-    strcpy(new_key, key);
-    strncat(new_key, text, n - strlen(key));
-    for(int i = 0; i < n; i++) {
-        text[i] = ((text[i] - 'A' + new_key[i] - 'A') % 26) + 'A';
+
+void autoclaveEncrypt(char* text, char* key) {
+    int i;
+    char fullKey[200];
+    strcpy(fullKey, key);
+    strcat(fullKey, text);
+    for(i = 0; text[i] != '\0'; i++) {
+        if(text[i] >= 'A' && text[i] <= 'Z')
+            text[i] = ((text[i] - 'A' + (fullKey[i] - 'A')) % 26) + 'A';
+        else if(text[i] >= 'a' && text[i] <= 'z')
+            text[i] = ((text[i] - 'a' + (fullKey[i] - 'a')) % 26) + 'a';
     }
 }
+
 int main() {
-    char text[] = "HELLO";
-    char key[] = "K";
-    autokeyEncrypt(text, key);
+    char text[100], key[100];
+    printf("Enter text to encrypt: ");
+    fgets(text, sizeof(text), stdin);
+    text[strcspn(text, "\n")] = '\0';
+    printf("Enter initial key: ");
+    fgets(key, sizeof(key), stdin);
+    key[strcspn(key, "\n")] = '\0';
+    autoclaveEncrypt(text, key);
     printf("Encrypted: %s\n", text);
     return 0;
 }
