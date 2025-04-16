80d46347b7cb878c0f590e3ce9938b988996d1a8
athirag30 <athirag3011@gmail.com>
2025-04-16 11:50:29 +0530

    Update railfence.c

--- railfence.c
+++ railfence.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-void railFenceEncrypt(char* text, int rails) {
-    int len = strlen(text);
-    char rail[rails][len];
-    for(int i = 0; i < rails; i++)
-        for(int j = 0; j < len; j++)
-            rail[i][j] = '\n';
-    int dir_down = 0, row = 0, col = 0;
-    for(int i = 0; i < len; i++) {
-        if(row == 0 || row == rails - 1)
-            dir_down = !dir_down;
-        rail[row][col++] = text[i];
-        dir_down ? row++ : row--;
-    }
-    for(int i = 0; i < rails; i++)
-        for(int j = 0; j < len; j++)
-            if(rail[i][j] != '\n')
-                printf("%c", rail[i][j]);
-    printf("\n");
+
+void railFenceEncrypt(char* text, char* result) {
+    int len = strlen(text), k = 0;
+    for(int i = 0; i < len; i += 2)
+        result[k++] = text[i];
+    for(int i = 1; i < len; i += 2)
+        result[k++] = text[i];
+    result[k] = '\0';
 }
+
 int main() {
-    char text[] = "HELLO";
-    int rails = 2;
-    railFenceEncrypt(text, rails);
+    char text[100], result[100];
+    printf("Enter text to encrypt: ");
+    fgets(text, sizeof(text), stdin);
+    text[strcspn(text, "\n")] = '\0';
+    railFenceEncrypt(text, result);
+    printf("Encrypted: %s\n", result);
     return 0;
 }
