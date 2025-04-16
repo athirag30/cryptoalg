b924715845e14037f035767301171a2df06fc202
athirag30 <athirag3011@gmail.com>
2025-04-16 11:48:30 +0530

    Update gronsfeld.c

--- gronsfeld.c
+++ gronsfeld.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 #include <string.h>
-void gronsfeldEncrypt(char* text, int* key, int key_len) {
+
+void gronsfeldEncrypt(char* text, char* key) {
+    int keyLen = strlen(key), j = 0;
     for(int i = 0; text[i] != '\0'; i++) {
-        if(text[i] >= 'A' && text[i] <= 'Z')
-            text[i] = ((text[i] - 'A' + key[i % key_len]) % 26) + 'A';
+        if(text[i] >= 'A' && text[i] <= 'Z') {
+            int shift = key[j % keyLen] - '0';
+            text[i] = ((text[i] - 'A' + shift) % 26) + 'A';
+            j++;
+        } else if(text[i] >= 'a' && text[i] <= 'z') {
+            int shift = key[j % keyLen] - '0';
+            text[i] = ((text[i] - 'a' + shift) % 26) + 'a';
+            j++;
+        }
     }
 }
+
 int main() {
-    char text[] = "HELLO";
-    int key[] = {1, 2, 3};
-    gronsfeldEncrypt(text, key, 3);
+    char text[100], key[100];
+    printf("Enter text to encrypt: ");
+    fgets(text, sizeof(text), stdin);
+    text[strcspn(text, "\n")] = '\0';
+    printf("Enter numeric key: ");
+    fgets(key, sizeof(key), stdin);
+    key[strcspn(key, "\n")] = '\0';
+    gronsfeldEncrypt(text, key);
     printf("Encrypted: %s\n", text);
     return 0;
 }
