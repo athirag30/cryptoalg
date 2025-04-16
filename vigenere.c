2095266666914264b89951c9d05daf0c4e2e7624
athirag30 <athirag3011@gmail.com>
2025-04-16 11:47:57 +0530

    Update vigenere.c

--- vigenere.c
+++ vigenere.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 #include <string.h>
+
 void vigenereEncrypt(char* text, char* key) {
-    int n = strlen(text), m = strlen(key);
-    for(int i = 0; i < n; i++) {
-        if(text[i] >= 'A' && text[i] <= 'Z')
-            text[i] = ((text[i] - 'A' + (key[i % m] - 'A')) % 26) + 'A';
+    int keyLen = strlen(key), j = 0;
+    for(int i = 0; text[i] != '\0'; i++) {
+        char k = key[j % keyLen];
+        if(text[i] >= 'A' && text[i] <= 'Z') {
+            text[i] = ((text[i] - 'A' + (k - 'A')) % 26) + 'A';
+            j++;
+        } else if(text[i] >= 'a' && text[i] <= 'z') {
+            text[i] = ((text[i] - 'a' + (k - 'a')) % 26) + 'a';
+            j++;
+        }
     }
 }
+
 int main() {
-    char text[] = "HELLO";
-    char key[] = "KEY";
+    char text[100], key[100];
+    printf("Enter text to encrypt: ");
+    fgets(text, sizeof(text), stdin);
+    text[strcspn(text, "\n")] = '\0';
+    printf("Enter key: ");
+    fgets(key, sizeof(key), stdin);
+    key[strcspn(key, "\n")] = '\0';
     vigenereEncrypt(text, key);
     printf("Encrypted: %s\n", text);
     return 0;
