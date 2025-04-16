0f218f3fa9d8da467bea9cee5448d0db06500e9e
athirag30 <athirag3011@gmail.com>
2025-04-16 11:39:00 +0530

    Update affine.c

--- affine.c
+++ affine.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+
 char affineEncryptChar(char ch, int a, int b) {
     if(ch >= 'A' && ch <= 'Z')
         return ((a * (ch - 'A') + b) % 26) + 'A';
+    else if(ch >= 'a' && ch <= 'z')
+        return ((a * (ch - 'a') + b) % 26) + 'a';
     return ch;
 }
+
 int main() {
-    char text[] = "HELLO";
+    char text[100];
     int a = 5, b = 8;
-    for(int i = 0; text[i] != '\0'; i++)
+
+    printf("Enter text to encrypt: ");
+    fgets(text, sizeof(text), stdin);
+    text[strcspn(text, "\n")] = '\0';
+
+    for(int i = 0; text[i] != '\0'; i++) {
         text[i] = affineEncryptChar(text[i], a, b);
+    }
+
     printf("Encrypted: %s\n", text);
     return 0;
 }
